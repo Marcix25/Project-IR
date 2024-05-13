528df8e34d69f9875b11edc9d5495e25e2665216
Mohamed Mahmoud <62149629+Marcix25@users.noreply.github.com>
2024-05-13 23:04:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -20,66 +20,7 @@ function searchProducts() {
         }
     });
 }
-function menutoggle() {
-    var MenuItems = document.getElementById('MenuItems');
-    if (MenuItems.style.maxHeight === '0px') {
-        MenuItems.style.maxHeight = '200px';
-    } else {
-        MenuItems.style.maxHeight = '0px';
-    }
-}
 
-function sortProducts() {
-    var selectElement = document.getElementById("sortingOptions");
-    var selectedValue = selectElement.value;
-
-    var products = document.querySelectorAll(".col-4");
-    var sortedProducts = Array.from(products);
-
-    switch (selectedValue) {
-        case "price":
-            sortedProducts.sort((a, b) => {
-                var priceA = parseFloat(a.querySelector("p").innerText.split(" ")[0]);
-                var priceB = parseFloat(b.querySelector("p").innerText.split(" ")[0]);
-                return priceA - priceB;
-            });
-            break;
-        case "popularity":
-            sortedProducts.sort((a, b) => {
-                // Generate random popularity values between 0 and 100
-                var popularityA = Math.floor(Math.random() * 101);
-                var popularityB = Math.floor(Math.random() * 101);
-                return popularityB - popularityA;
-            });
-            break;
-        case "rating":
-            sortedProducts.sort((a, b) => {
-                // Generate random rating values between 0 and 5
-                var ratingA = Math.random() * 5;
-                var ratingB = Math.random() * 5;
-                return ratingB - ratingA;
-            });
-            break;
-        case "sale":
-            sortedProducts.sort((a, b) => {
-                // Generate random sale values between 0 and 50 (representing percentage discount)
-                var saleA = Math.floor(Math.random() * 51);
-                var saleB = Math.floor(Math.random() * 51);
-                return saleB - saleA;
-            });
-            break;
-        default:
-            // No sorting, do nothing
-            break;
-    }
-
-    var container = document.querySelector(".row");
-    container.innerHTML = "";
-
-    sortedProducts.forEach(product => {
-        container.appendChild(product);
-    });
-}
 
 document.getElementById("loginForm").addEventListener("submit", function(event) {
     event.preventDefault();
